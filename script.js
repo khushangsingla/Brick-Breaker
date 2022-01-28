e60a91d3d111be4a3f7970087c7fccda62799b6e
Khushang Singla <khushangsingla@gmail.com>
2022-01-29 00:31:58 +0530

    Most of the major features are updated properly. The canvas to be rendered after victory is yet to be updated. Moreover, in class Brick, function isBallColliding is to be implemented properly to ensure that ball was going towards the brick before assuming that brick and ball really collide.

--- script.js
+++ script.js
@@ -16,38 +16,39 @@ ct.canvas.height = frame_y;
 const WINDOW_Y=frame_y;
 const WINDOW_X=frame_x;
 const WINDOW_COLOR='rgb(230,230,230)';
-const STEP_TIME=1/*val left */;
+const STEP_TIME=1;
 //Bullet
-const BULLET_SPEED=1/*val left */ ;
-const BULLET_HEIGHT=1/*val left */ ;
-const BULLET_WIDTH=1/*val left */ ;
+const BULLET_SPEED=0.003*frame_y ;
+const BULLET_HEIGHT=0.01*frame_x ;
+const BULLET_WIDTH=0.0075*frame_x ;
 const BULLET_COLOR="rgb(256,0,0)";
 //Brick
-const BRICK_HEIGHT=0.05*frame_x/*val left */;
-const BRICK_WIDTH=0.1*frame_x/*val left */;
+const BRICK_HEIGHT=0.04*frame_x;
+const BRICK_WIDTH=0.08*frame_x;
 let STRONG_BRICK_COLOR='rgb(255,0,0)';
 let WEAK_BRICK_COLOR='rgb(155,0,0)';
 //Collectible(Falling)
-const COLLECTIBLE_RADIUS=1/*val left */;
-const COLLECTIBLE_COLOR="rgb(256,0,0)"/*val left */;
-const COLLECTIBLE_SPEED=1/*val left */;
+const COLLECTIBLE_RADIUS=0.015*frame_x;
+const COLLECTIBLE_COLOR="rgb(256,0,0)";
+const COLLECTIBLE_SPEED=0.004*frame_y;
+const COLLECTIBLE_TYPE_COLOR='rgb(0,255,0)';
 //Paddle
-const PADDLE_HEIGHT=0.02*frame_y/*val left */;
-const PADDLE_WIDTH=0.1*frame_x/*val left */;
-const PADDLE_STEP_SPEED=0.005*frame_x/*val left */;
-const FAST_PADDLE_STEP_SPEED=0.01*frame_x/*val left */;
-const LONG_PADDLE_WIDTH=1/*val left */;
-const SHORT_PADDLE_WIDTH=1/*val left */;
+const PADDLE_HEIGHT=0.02*frame_y;
+const PADDLE_WIDTH=0.1*frame_x;
+const PADDLE_STEP_SPEED=0.005*frame_x;
+const FAST_PADDLE_STEP_SPEED=0.01*frame_x;
+const LONG_PADDLE_WIDTH=0.15*frame_x;
+const SHORT_PADDLE_WIDTH=0.07*frame_x;
 const PADDLE_COLOR='rgb(0,0,255)';
 //Ball
-const BALL_SPEED_Y=frame_y/200/*val left */;
-const SLOW_BALL_FACTOR=0.8/*val left */;
-const BALL_RADIUS=0.01*frame_x/*val left */;
-const BALL_COLOR_OUT='rgb(20,20,20)'/*val left */;
-const BALL_COLOR_IN='rgb(180,180,180)'/*val left */;
-
+const BALL_SPEED_Y=frame_y/100;
+const SLOW_BALL_FACTOR=0.8;
+const BALL_RADIUS=0.01*frame_x;
+const BALL_COLOR_OUT='rgb(20,20,20)';
+const BALL_COLOR_IN='rgb(180,180,180)';
+const BALL_DISPERSAL_SPEED=0.2*BALL_SPEED_Y;
 //throw direction
-const THROW_LINE_LENGTH=0.05*frame_y/*val left */;
+const THROW_LINE_LENGTH=0.05*frame_y;
 const MAX_THROW_ANGLE=80;//in degrees;
 const THROW_LINE_COLOR='rgb(255,0,0)';
 //Required variables
@@ -57,16 +58,19 @@ let balls=[]; //array of balls in the game
 let availableCollectibles=[]; //array of collectibles that do exist in the game
 let fallingCollectibles=[]; //array of collectibles that are released and are falling
 let activeCollectible=null; //if there's an active collectible, it's initial will be stored here
+let collectibleTimeRemaining=0;
+let featureTimeRemaining=0;
+let activeFeature=null;
 let numberOfLeft=0; //number of continuous left movements made
 let numberOfRight=0; //number of continuous right movements made
 let activeBullets=[]; //this is array of bullets that are released and are yet to hit a brick
 let numberOfBulletsAvailable=0; //this is number of bullets available with player
 let caught=true; //if the ball is caught by paddle, it is true
 let caughtBallIndex=0;
-let catchCount=0; //this has the number of catch counts available with the player
+let catchCount=4; //this has the number of catch counts available with the player
 let Life=3; //Total life of player
 let Score=0; //Total Score
-let wrap=false;
+let wrap=true;
 //Class for vectors
 class Vector{
     constructor(x,y){
@@ -76,6 +80,10 @@ class Vector{
     sum(vec=new Vector()){//for A and B to be two vectors, A.sum(B) is equivalent to A+=B
         this.x+=vec.x;
         this.y+=vec.y;
+    }
+    getSum(vec=new Vector()){
+        let ans=new Vector(this.x+vec.x,this.y+vec.y);
+        return ans;
     }
     product(fac){//scalar multiplication of n by vector
         this.x=fac*this.x;
@@ -153,7 +161,8 @@ class Ball extends MovingObject{
         // console.log(this.velocity.y)
     }
     slowSpeed(){
-        this.velocity/=SLOW_BALL_FACTOR;
+        this.velocity.x*=SLOW_BALL_FACTOR;
+        this.velocity.y*=SLOW_BALL_FACTOR;
     }
     normalSpeed(){
         this.velocity*=SLOW_BALL_FACTOR;
@@ -176,6 +185,10 @@ class Ball extends MovingObject{
         ctx.fill();
         ctx.closePath();
     }
+    disperse(array_balls){
+        array_balls.push(new Ball(this.pos.getSum(new Vector(-1*BALL_RADIUS,0)),this.velocity.getSum(new Vector(-1*BALL_DISPERSAL_SPEED,0))));
+        array_balls.push(new Ball(this.pos.getSum(new Vector(BALL_RADIUS,0)),this.velocity.getSum(new Vector(BALL_DISPERSAL_SPEED,0))));
+    }
 }
 
 class Brick extends StaticObject{
@@ -185,7 +198,6 @@ class Brick extends StaticObject{
     }
     isBallColliding(ball=new Ball()){
         if(ball.right >= this.left && ball.left <= this.right && ball.bottom >= this.top && ball.top <= this.bottom){
-            console.log(ball.top,this.bottom);
             this.brickLife-=1;
             let dist_along_x=Math.abs(ball.pos.x-this.pos.x);
             let dist_along_y=Math.abs(ball.pos.y-this.pos.y);
@@ -259,10 +271,10 @@ class Paddle extends StaticObject{
         }
         if(is_wrap){
             this.pos.sum(this.stepDistance.getProduct(dir));
-            if(this.pos.x<=0){
+            if(this.pos.x<0){
                 this.pos.x=WINDOW_X+this.pos.x;
             }
-            else if(this.pos.x>WINDOW_Y){
+            else if(this.pos.x>WINDOW_X){
                 this.pos.x-=WINDOW_X;
             }
             this.refreshContents();
@@ -306,7 +318,7 @@ class Paddle extends StaticObject{
 
 class Bullet extends MovingObject{
     constructor(pos=new Vector()){
-        super(pos,new Vector(BULLET_WIDTH,BULLET_HEIGHT),BULLET_COLOR,new Vector(0,BULLET_SPEED));
+        super(pos,new Vector(BULLET_WIDTH,BULLET_HEIGHT),BULLET_COLOR,new Vector(0,-1*BULLET_SPEED));
     }
 }
 
@@ -322,24 +334,28 @@ class FallingCollectible extends MovingObject{
         super(pos,new Vector(2*COLLECTIBLE_RADIUS,2*COLLECTIBLE_RADIUS),COLLECTIBLE_COLOR,new Vector(0,COLLECTIBLE_SPEED));
         this.type=type;
     }
-    isCollected(paddle){
-        if(this.bottom>paddle.top && this.right>paddle.left && this.left<paddle.right && this.top>paddle.bottom){
+    isCollected(paddle=new Paddle()){
+        if(this.bottom>paddle.top && this.right>paddle.left && this.left<paddle.right && this.top<paddle.bottom){
             return true;
         }
+        return false;
     }
     reachedBottom(){
         return (this.top>=WINDOW_Y);
     }
     render(ctx){
-        grad=ctx.createRadialGradient(this.pos.x,this.pos.y,COLLECTIBLE_RADIUS*0.8,this.pos.x,this.pos.y,COLLECTIBLE_RADIUS)
-        ctx.addColorStop(0,WINDOW_COLOR);
-        ctx.addColorStop(1,rgb(230,230,230));
-        ctx.fillStyle=grad;
         ctx.beginPath();
+        let grad=ctx.createRadialGradient(this.pos.x,this.pos.y,COLLECTIBLE_RADIUS*0.8,this.pos.x,this.pos.y,COLLECTIBLE_RADIUS)
+        grad.addColorStop(0,WINDOW_COLOR);
+        grad.addColorStop(1,COLLECTIBLE_COLOR);
+        ctx.fillStyle=grad;
         ctx.arc(this.pos.x,this.pos.y,COLLECTIBLE_RADIUS,0,2*Math.PI);
+        ctx.fill();
         ctx.closePath();
-        ctx.fillStyle='rgb(0,255,0)';
-        ctx.fillText(type,this.pos.x,this.pos.y,1.2*COLLECTIBLE_RADIUS);
+        ctx.beginPath();
+        ctx.fillStyle=COLLECTIBLE_TYPE_COLOR;
+        ctx.fillText(this.type,this.pos.x,this.pos.y,1.4*COLLECTIBLE_RADIUS);
+        ctx.closePath();
     }
 }
 
@@ -378,6 +394,7 @@ class Ground{
     }
 }
 
+
 //initial setting up
 let playArea=new Ground;
 playArea.render(ct);
@@ -386,20 +403,130 @@ player.render(ct);
 balls.push(new Ball(new Vector(player.pos.x,player.top-BALL_RADIUS),new Vector(0,0)));
 balls[0].render(ct);
 let throwLine=new ThrowDirection(balls[0].pos);
-// throwLine.render(ct,balls[caughtBallIndex]);
-bricks.push(new Brick(new Vector(frame_x/2,frame_y/2),2,STRONG_BRICK_COLOR));
-// bricks[0].render(ct);
-// console.log(player.bottom);
-// let player=new Paddle(WINDOW_X/2.0,WINDOW_Y);
+// bricks.push(new Brick(new Vector(frame_x/2,frame_y/2),2,STRONG_BRICK_COLOR));//for testing
+// bricks.push(new Brick(new Vector(frame_x/4,frame_y/4),2,STRONG_BRICK_COLOR));//for testing
+// availableCollectibles.push(new EnclosedCollectible('M',0));//for testing
+
+function displayLostScreen(score,ctx){
+    //pending fn
+}
+function displayWinScreen(score,ctx){
+    //pending fn
+}
+function collectibleAction(type){
+    if(type=='L'){
+        activeCollectible=type;
+        player.longLength();
+        collectibleTimeRemaining=10;
+        // setTimeout(endActiveCollectibleAction('L'),10000);
+    }
+    else if(type=='S'){
+        activeCollectible=type;
+        player.shortLength();
+        collectibleTimeRemaining=10;
+        // setTimeout(endActiveCollectibleAction('L'),10000);
+    }
+    else if(type=='B'){
+        numberOfBulletsAvailable+=3;
+    }
+    else if(type=='W'){
+        activeCollectible=type;
+        wrap=true;
+        collectibleTimeRemaining=10;
+    }
+    else if(type=='R'){
+        activeCollectible=type;
+        collectibleTimeRemaining=10;
+    }
+    else if(type=='C'){
+        catchCount+=3;
+    }
+    else if(type=='M'){
+        let number_of_balls=balls.length
+        for(let i=0;i<number_of_balls;i++){
+            balls[i].disperse(balls);
+        }
+    }
+}
+function endActiveCollectibleAction(type){
+    activeCollectible=null;
+    if(type=='L' || type=='S'){
+        player.normalLength();
+        return;
+    }
+    if(type=='W'){
+        wrap=false;
+    }
+}
 function update(){
+    console.log(player.stepDistance.x)
+    //timers
+    collectibleTimeRemaining-=0.01;
+    featureTimeRemaining-=0.01;
+    //action of timers
+    if(activeCollectible!=null && collectibleTimeRemaining<=0){
+        endActiveCollectibleAction(activeCollectible);
+    }
+    if(activeFeature!=null && featureTimeRemaining<=0){
+        if(activeFeature=='F'){
+            player.normalStepSpeed();
+        }
+        if(activeFeature=='S'){
+            for(let i=0;i<balls.length;i++){
+                balls[i].normalSpeed();
+            }
+        }
+    }
+    //rendering objects
     playArea.render(ct);
     player.render(ct);
+    let LivingBricks=0;
     for(let j=0;j<bricks.length;j++){
         if(bricks[j].brickLife!=0){
             bricks[j].render(ct);
+            LivingBricks++;
         }
     }
-    // if(balls.pos.y>WINDOW_Y)
+    for(let i=0;i<fallingCollectibles.length;i++){
+        fallingCollectibles[i].render(ct);
+    }
+    for(let i=0;i<activeBullets.length;i++){
+        activeBullets[i].render(ct);
+    }
+    for(let i=0;i<balls.length;i++){
+        balls[i].render(ct);
+    }
+    
+    //check for game-end
+    if(!LivingBricks){
+        clearInterval(GAME_RUN);
+        displayWinScreen(Score,ct);
+    }
+    if(Life==0){
+        clearInterval(GAME_RUN);
+        displayLostScreen(Score,ct);
+    }
+    for(let i=0;i<activeBullets.length;i++){
+        activeBullets[i].updatePos();
+    }
+    for(let i=0;i<fallingCollectibles.length;i++){
+        if(fallingCollectibles[i].reachedBottom()){
+            fallingCollectibles.splice(i,1);
+            i--;
+            continue;
+        }
+        if(fallingCollectibles[i].isCollected(player)){
+            if(activeCollectible!=null){
+                endActiveCollectibleAction(activeCollectible);
+            }
+            collectibleAction(fallingCollectibles[i].type);
+            fallingCollectibles.splice(i,1);
+            i--;
+            continue;
+        }
+        fallingCollectibles[i].updatePos();
+    }
+    // check if balls finished
     if(balls.length==0){
         Life--;
         balls.push(new Ball(new Vector(player.pos.x,player.top-BALL_RADIUS),new Vector(0,0)));
@@ -408,9 +535,11 @@ function update(){
         throwLine.startPosition=balls[0].pos;
         throwLine.angle=0;
     }
+    //updating data
     if(caught){
         // console.log('doign');
         throwLine.render(ct);
+        return;
     }
     
     for(let i=0;i<balls.length;i++){
@@ -419,13 +548,21 @@ function update(){
             i--;
             continue;
         }
-        balls[i].render(ct)
         player.collisionWithBall(balls[i],throwLine);
         for(let j=0;j<bricks.length;j++){
             if(bricks[j].brickLife!=0){
                 // bricks[j].render(ct);
                 if(bricks[j].isBallColliding(balls[i])){
                     Score++;
+                    if(bricks[j].brickLife==0){
+                        for(let k=0;k<availableCollectibles.length;k++){
+                            if(availableCollectibles[k].brickIndex==j){
+                                fallingCollectibles.push(new FallingCollectible(bricks[j].pos,availableCollectibles[k].type));
+                                availableCollectibles.splice(k,1);
+                                break;
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -447,32 +584,61 @@ function keyPressed(code){
         if(caught){
             balls[caughtBallIndex].throwTheBall(throwLine);
         }
+        if(numberOfBulletsAvailable!=0){
+            activeBullets.push(new Bullet(new Vector(player.pos.x,player.pos.y)));
+            numberOfBulletsAvailable--;
+        }
     }
-    if(code=='a' || code=='A' || code=='ArrowLeft'){
+    else if(code=='a' || code=='A' || code=='ArrowLeft'){
         if(!caught){
-            numberOfLeft++;
-            numberOfRight=0;
-            player.move(-1,0);
+            if(activeCollectible=='R'){
+                numberOfLeft=0;
+                numberOfRight++;
+                player.move(1,wrap);
+            }
+            else{
+                numberOfLeft++;
+                numberOfRight=0;
+                player.move(-1,wrap);
+            }
         }
         else{
             throwLine.moveLeft();
         }
     }
-    if(code=='d' || code=='D' || code=='ArrowRight'){
+    else if(code=='d' || code=='D' || code=='ArrowRight'){
         if(!caught){
-            numberOfLeft=0;
-            numberOfRight++;
-            player.move(1,0);
+            if(activeCollectible=='R'){
+                numberOfLeft++;
+                numberOfRight=0;
+                player.move(-1,wrap);
+            }
+            else{
+                numberOfLeft=0;
+                numberOfRight++;
+                player.move(1,wrap);
+            }
         }
         else{
             throwLine.moveRight();
         }
     }
+    else if(code=='f' || code=='F'){
+        player.fastStepSpeed();
+        featureTimeRemaining=10;
+        activeFeature='F';
+    }
+    else if(code=='s' || code=='S'){
+        for(let i=0;i<balls.length;i++){
+            balls[i].slowSpeed();
+        }
+    }
 }
-// ct.strokeStyle='red';
-// ct.moveTo(0,0);
-// ct.lineTo(400,400);
-// ct.stroke();
-
-setInterval(update,1);
+const GAME_RUN=setInterval(update,1);
 document.onkeydown=(evt)=>keyPressed(evt.key);
+document.getElementById('fast_paddle_button').onclick = ()=>{
+    keyPressed('F');
+}
+document.getElementById('slow_ball_button').onclick = ()=>{
+    keyPressed('S');
+}
